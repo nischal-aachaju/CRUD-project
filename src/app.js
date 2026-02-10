ba84d1d3057df9003710d3f9c1ac767f08ac75da
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-10 18:27:32 +0545

    post and get complected

--- src/app.js
+++ src/app.js
@@ -23,6 +23,14 @@ app.post("/create-post",upload.single("image") ,async(req,res)=>{
         post:post
     })
 })
+app.get("/posts",async( req,res)=>{
 
+    const posts= await postModel.find()
+    return res.status(200).json({
+        message:"posted data",
+        posts
+
+    })
+})
 
 module.exports=app
