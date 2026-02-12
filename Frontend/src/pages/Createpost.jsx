680b9aee83581513cd79c9c65b1661d3fc5c1bf3
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-12 20:57:19 +0545

    home section, bacis

--- Frontend/src/pages/Createpost.jsx
+++ Frontend/src/pages/Createpost.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Createpost = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center gap-3 '>
-
+    <div className='h-screen w-full flex justify-center items-center gap-3 flex-col '>
+              <Link className="bg-green-600 rounded ml-4   px-2 py-1" to="/">Home </Link>
       <form className='bg-gray-300 w-80%  h-140 flex  flex-col px-20 py-20 gap-10 rounded ' action="post">
         <h1 className='inline-block  w-full py-2  text-center mt-4 text-3xl font-serif font-black '>Create post</h1>
         <label className=' text-2xl capitalize'>upload image:</label>
@@ -11,6 +11,8 @@ const Createpost = () => {
         <input className='bg-white/70 border rounded px-3 py-1 text-xl'  type="text" required placeholder='caption' />
         <button className=' bg-green-500 rounded w-fit py-2 px-3 text-white'>Submit</button>
       </form>
+
+
     </div>
   )
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,30 @@
-
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return (
-    <div>
-      home page
-    </div>
-  )
-}
+  
+  const [post, setPost] = useState([
+    { _id: 1,
+      image: "https://www.mamp.one/wp-content/uploads/2024/09/image-resources2.jpg",
+      caption: "helllo" },
+  ]);
 
-export default Home
+  return (
+    <div className="h-screen w-full pt-3">
+      <Link className="bg-green-600 rounded ml-4    px-2 py-1" to="/create-post">
+        Create post
+      </Link>
+      <div>
+        {post.map((e) => {
+          return (
+            <div className="h-50 mt-3 relative">
+              <img  className="h-full"src={e.image} alt="image" />
+              <p>{e.caption}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
