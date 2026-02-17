996d76d4666d464a92a8ca978e7b5d96efaa8a03
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-17 11:22:10 +0545

    sercer error fixing

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,6 @@ const connectDB=require("./src/db/db")
 
 connectDB()
 
-const PORT = process.env.PORT || 3000;
-
 //starts the server
 app.listen(3000,()=>{
     console.log("server is running on port 3000 .....");
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://YOUR-FRONTEND.onrender.com"],
+    origin:"http://localhost:5173",
   }),
 );
 app.use(express.json());
